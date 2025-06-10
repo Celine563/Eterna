c82d8e8c4fe845a24991b873a78928679ffa0ef0
Celine <B00160080@mytudublin.ie>
2025-06-10 15:39:53 +0100

    title

---
+++ V1/HomePage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="CSS/HomeStyle.css">
+
+</head>
+
+<body>
+
+
+<div class="categories">
+<h1>ETERNA</h1>
+  <h3>Women</h3>
+  <h3>Men</h3>
+  <h3>Children</h3>
+  <h3>Bags</h3>
+  <h3>Beauty</h3>
+  <h3>Our Story</h3>
+  <img src="Images/Search.png" alt="search" class="search">
+  <img src="Images/Account.png" alt="account" class="account">
+</div>
+
+
+
+</body>
+</html>
